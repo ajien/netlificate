25efcd6490101c22f515abd00eeb3a15a6777574
Aaron Jeffrey Nolasco <ajie.nolasco@gmail.com>
2019-01-09 01:07:47 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  {
-    "format_on_save": true
   }
 }
