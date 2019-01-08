70dd20d21ece539845ef3266ca2860f8d0085de3
Aaron <ajie.nolasco@gmail.com>
2019-01-08 23:40:08 +0800

    update

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,79 +1,69 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"/>
-    <div class="Triangle Triangle--one"/>
-    <div class="Triangle Triangle--three"/>
-    <div class="Triangle Triangle--four"/>
+  <div class="logo">
+    <svg
+      id="logo.svg"
+      xmlns="http://www.w3.org/2000/svg"
+      width="282"
+      height="282"
+      viewBox="0 0 282 282"
+    >
+      <defs></defs>
+      <circle
+        id="Ellipse_1_copy_2"
+        data-name="Ellipse 1 copy 2"
+        class="cls-1"
+        cx="141"
+        cy="141"
+        r="137"
+      ></circle>
+      <circle
+        id="Ellipse_1_copy_3"
+        data-name="Ellipse 1 copy 3"
+        class="cls-2"
+        cx="141.5"
+        cy="141.5"
+        r="129.5"
+      ></circle>
+      <path
+        id="W_copy_3"
+        data-name="W copy 3"
+        class="cls-3"
+        d="M485.062,375v95.209q0,11.58-6.472,11.579-3.453,0-4.637-2.358t-1.187-9.221V375H428.978v95.959q0,10.832-6.471,10.829-3.453,0-4.584-2.251t-1.132-9.328V375H373V476.747q0,23.376,10.57,35.811T413.986,525q12.512,0,21.032-4.771T450.98,504.3Q463.058,525,485.924,525q18.983,0,30.954-12.437t11.972-32.165V375H485.062Z"
+        transform="translate(-337 -309)"
+      ></path>
+      <path
+        id="R_copy_3"
+        data-name="R copy 3"
+        class="cls-4"
+        d="M537.1,414.347q15.96,0,15.962,10.4,0,11.795-13.8,11.794H535.8v37.633L557.05,521.35H603l-25.237-60.364q19.519-15.224,19.521-37.419,0-21.764-15.747-35.918Q567.4,375,541.735,375H485.543l43.789,39.349H537.1Z"
+        transform="translate(-337 -309)"
+      ></path>
+    </svg>
   </div>
 </template>
 
-<style>
-.VueToNuxtLogo {
-  display: inline-block;
-  animation: turn 2s linear forwards 1s;
-  transform: rotateX(180deg);
-  position: relative;
-  overflow: hidden;
-  height: 180px;
-  width: 245px;
-}
-
-.Triangle {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 0;
-  height: 0;
-}
-
-.Triangle--one {
-  border-left: 105px solid transparent;
-  border-right: 105px solid transparent;
-  border-bottom: 180px solid #41b883;
-}
-
-.Triangle--two {
-  top: 30px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 87.5px solid transparent;
-  border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3b8070;
-}
-
-.Triangle--three {
-  top: 60px;
-  left: 35px;
-  animation: goright 0.5s linear forwards 3.5s;
-  border-left: 70px solid transparent;
-  border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495e;
-}
-
-.Triangle--four {
-  top: 120px;
-  left: 70px;
-  animation: godown 0.5s linear forwards 3s;
-  border-left: 35px solid transparent;
-  border-right: 35px solid transparent;
-  border-bottom: 60px solid #fff;
-}
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
+    <style>
+  .cls-1,
+  .cls-3 {
+    fill: #fff;
   }
-}
 
-@keyframes godown {
-  100% {
-    top: 180px;
+  .cls-1 {
+    stroke: #575964;
+    stroke-linejoin: round;
+    stroke-width: 4px;
   }
-}
 
-@keyframes goright {
-  100% {
-    left: 70px;
+  .cls-2 {
+    fill: #575964;
   }
-}
+
+  .cls-3,
+  .cls-4 {
+    fill-rule: evenodd;
+  }
+
+  .cls-4 {
+    fill: #ffa200;
+  }
 </style>
